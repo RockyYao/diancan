ab3795e0951fb616df8177a555cad051e8eb4d13
rockyyao work <770166662@qq.com>
2018-08-16 14:27:12 +0800

    Initial commit

---
+++ src/main/java/com/oechsler/diancan/DiancanApplication.java
@@ -0,0 +1,12 @@
+package com.oechsler.diancan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiancanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiancanApplication.class, args);
+	}
+}
